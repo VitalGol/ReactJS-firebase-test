28248d1468065cf7627bc85eca54bbcfda81a7e2
Vitaly <vitalgolob@gmail.com>
2020-05-21 21:03:17 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,119 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import * as firebase from "firebase";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      hasAccount: false,
+      name: "",
+      key: "",
+      value: "",
+    };
+  }
+
+  componentDidMount() {
+    const db = firebase.database();
+    // console.log(db);
+
+    const name = db.ref("name");
+    // console.log(name);
+
+    // name.on("value", (elem) => {
+    //   console.log(">>", elem.val());
+    // });
+    name.on("value", (elem) => {
+      this.setState({ name: elem.val() });
+    });
+  }
+
+  handleChange = ({ target: { value, id } }) => {
+    // console.log("id--", id);
+    // console.log("value--", value);
+    this.setState({ [id]: value });
+  };
+
+  createAccount = () => {
+    const { email, password } = this.state;
+
+    //it's registration
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch((error) => console.error(error));
+
+    //it's inter
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((response) => {
+        console.log();
+        this.setState({ hasAccount: true });
+      })
+      .catch((error) => console.error(error));
+  };
+
+  sendData = () => {
+    const { key, value } = this.state;
+    const db = firebase.database();
+    db.ref(key).push(value);
+    console.log("db");
+  };
+
+  render() {
+    // console.log(this.state);
+    // console.log("hasAccount--", this.state.hasAccount);
+    // const { hasAccount } = this.state;
+    const { hasAccount, name } = this.state;
+    console.log("-->>", name);
+    return (
+      <div className="container">
+        {hasAccount ? (
+          <div className="login">
+            <input
+              type="text"
+              id="key"
+              placeholder="enter key"
+              onChange={this.handleChange}
+            />
+            <input
+              type="text"
+              id="value"
+              placeholder="enter value"
+              onChange={this.handleChange}
+            />
+            <input
+              style={{ color: "blue" }}
+              type="submit"
+              onClick={this.sendData}
+            />
+          </div>
+        ) : (
+          <div className="login">
+            {/* <h1>TEST</h1> */}
+            <input
+              id="email"
+              type="text"
+              placeholder="email"
+              onChange={this.handleChange}
+            />
+            <input
+              id="password"
+              type="password"
+              placeholder="password"
+              onChange={this.handleChange}
+            />
+            <input
+              style={{ color: "red" }}
+              type="submit"
+              onClick={this.createAccount}
+            />
+          </div>
+        )}
+      </div>
+    );
+  }
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCMSRouimDWmPfcBGmtAi34I48Hu_K6AuM",
+  authDomain: "fir-project-5d738.firebaseapp.com",
+  databaseURL: "https://fir-project-5d738.firebaseio.com",
+  projectId: "fir-project-5d738",
+  storageBucket: "fir-project-5d738.appspot.com",
+  messagingSenderId: "315607246317",
+  appId: "1:315607246317:web:24b854af63ae48508fb3e4",
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
